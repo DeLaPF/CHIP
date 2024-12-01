b153151513982acb0ac2612eb98289c25ed6647c
DeLaPF <de.alafia@gmail.com>
2024-12-01 00:15:51 -0500

    rearrange

--- src/main.c
+++ src/main.c
@@ -7,19 +7,103 @@ const float frameThreshold = 1 / 60.0;
 const float soundThreshold = 1 / 60.0;
 const float delayThreshold = 1 / 60.0;
 
+const int PADDING = 1;
+const int DEBUG_INFO_WIDTH = 30;
+const int DEBUG_INFO_HEIGHT = 54;
+const int DEBUG_BUTTONS_WIDTH = 66;
+const int DEBUG_BUTTONS_HEIGHT = 19;
+
+const int BUFF_WIDTH = 64;
+const int BUFF_HEIGHT = 32;
+
 struct chip_8 {
     uint8_t registers[16];
-    uint8_t frameBuff[2048];
+    uint8_t pixelBuff[2048];
 };
 
-void checkered(uint8_t* frameBuff) {
-    for (int y = 0; y < 32; y++) {
-        for (int x = 0; x < 64; x++) {
+struct cpu {
+    uint8_t registers[16];
+    uint16_t idxReg;
+    uint8_t stack[64];
+    uint8_t stackPtr;
+    uint16_t programCtr;
+    uint8_t heap[4096];
+};
+
+void checkered(uint8_t* pixelBuff)
+{
+    for (int y = 0; y < BUFF_HEIGHT; y++) {
+        for (int x = 0; x < BUFF_WIDTH; x++) {
             if (((x%2)+(y%2)+1)%2) {
-                frameBuff[y*64+x] = 1;
+                pixelBuff[y*BUFF_WIDTH+x] = 1;
             }
         }
     }
+}
+
+void drawScreen(const uint8_t* pixelBuff)
+{
+    const double drawScale = 8.0;
+    const double padding = PADDING*drawScale;
+    BeginDrawing();
+        ClearBackground(GRAY);
+
+        // Draw Buff Frame
+        const Vector2 buffFrameOrigin = { padding, padding };
+        const double buffFrameWidth = (BUFF_WIDTH+2)*drawScale;
+        const double buffFrameHeight = (BUFF_HEIGHT+2)*drawScale;
+        DrawRectangle(
+            buffFrameOrigin.x, buffFrameOrigin.y,
+            buffFrameWidth, buffFrameHeight,
+            RAYWHITE
+        );
+
+        // Draw Debug Info Frame
+        const Vector2 debugInfoFrameOrigin = {
+            buffFrameOrigin.x+buffFrameWidth+padding,
+            padding
+        };
+        const double debugInfoFrameHeight = DEBUG_INFO_HEIGHT*drawScale;
+        const double debugInfoFrameWidth = DEBUG_INFO_WIDTH*drawScale;
+        DrawRectangle(
+            debugInfoFrameOrigin.x, debugInfoFrameOrigin.y,
+            debugInfoFrameWidth, debugInfoFrameHeight,
+            RAYWHITE
+        );
+
+        // Draw Debug Buttons Frame
+        const Vector2 debugButtonsFrameOrigin = {
+            padding,
+            buffFrameOrigin.y+buffFrameHeight+padding
+        };
+        const double debugButtonsFrameHeight = DEBUG_BUTTONS_HEIGHT*drawScale;
+        const double debugButtonsFrameWidth = DEBUG_BUTTONS_WIDTH*drawScale;
+        DrawRectangle(
+            debugButtonsFrameOrigin.x, debugButtonsFrameOrigin.y,
+            debugButtonsFrameWidth, debugButtonsFrameHeight,
+            RAYWHITE
+        );
+
+        // Draw Pixel Buffer to Buff Frame
+        const Vector2 pixelBuffOrigin = {
+            buffFrameOrigin.x+padding,
+            buffFrameOrigin.y+padding
+        };
+        for (int y = 0; y < BUFF_HEIGHT; y++) {
+            for (int x = 0; x < BUFF_WIDTH; x++) {
+                if (pixelBuff[y*BUFF_WIDTH+x]) {
+                    DrawRectangle(
+                        pixelBuffOrigin.x+(x*drawScale),
+                        pixelBuffOrigin.y+(y*drawScale),
+                        drawScale,
+                        drawScale,
+                        BLACK
+                    );
+                }
+            }
+        }
+
+    EndDrawing();
 }
 
 void draw()
@@ -37,23 +121,12 @@ int main(void)
     const int windowHeight = 450;
     InitWindow(windowWidth, windowHeight, "CHIP-8");
 
-    // Chip-8 screen
-    const Vector2 virtualScreenOrigin = { -10.0f, -10.0f };
-    const double drawScale = 8.0;
-    const double borderWidth = 1.0;
-    const Vector2 pixelOrigin = {
-        -virtualScreenOrigin.x+(borderWidth*drawScale),
-        -virtualScreenOrigin.y+(borderWidth*drawScale),
-    };
-    Rectangle virtualScreen = {
-        0.0f, 0.0f,
-        (64.0+(borderWidth*2))*drawScale, (32.0+(borderWidth*2))*drawScale
-    };
+    // Chip-8
     struct chip_8 chip8 = {
         { 0 },
         { 0 },
     };
-    checkered(chip8.frameBuff);
+    checkered(chip8.pixelBuff);
 
     // SetTargetFPS(60);
     double prevFrameTime = GetTime();
@@ -73,24 +146,7 @@ int main(void)
 
         // Handle intent
         if (willDrawFrame) {
-            // draw(&pixelBuff);
-            BeginDrawing();
-                ClearBackground(GRAY);
-                DrawRectanglePro(virtualScreen, virtualScreenOrigin, 0.0f, RAYWHITE);
-                for (int y = 0; y < 32; y++) {
-                    for (int x = 0; x < 64; x++) {
-                        if (chip8.frameBuff[y*64+x]) {
-                            DrawRectangle(
-                                pixelOrigin.x+(x*drawScale),
-                                pixelOrigin.y+(y*drawScale),
-                                drawScale,
-                                drawScale,
-                                BLACK
-                            );
-                        }
-                    }
-                }
-            EndDrawing();
+            drawScreen(chip8.pixelBuff);
         }
         if (willHandleSound) { handleSound(); }
     }
